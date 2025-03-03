ebf0c206e3e046d849bcdca608ef8380ba635969
nugroho <nugrohonug066@gmail.com>
2025-03-04 01:19:03 +0700

    adding more component on landing page & fixing some navbar

---
+++ src/components/LandingProductCard.js
@@ -0,0 +1,27 @@
+import Image from "next/image";
+const LandingProductCard = ({imgSrc, isNew = false, colorChoice, tiitle, price}) => {
+    return (
+        <>
+        <div className="col-span-1 flex flex-col gap-4">
+                                <div className="h-[28rem] relative">
+                                    <Image src={imgSrc} className="h-[28rem] object-cover object-center" width={1000} height={1000} alt="models1"/>
+                                    <div className="w-full h-full absolute inset-0 grid items-end p-4">
+                                        {
+                                             isNew && (
+                                                <span className="bg-white w-fit px-2 py-0.5 text-sm">
+                                                NEW
+                                                </span>
+                                             )
+                                        }
+                                    </div> 
+                                </div>
+                                <p className="text-center tracking-wider text-sm font-light ">{tiitle}</p>
+                                {
+                                    colorChoice
+                                }
+                                <p className="text-center tracking-widest text-sm font-light ">{price}</p>
+                        </div>
+        </>
+    )
+}
+export default LandingProductCard;
