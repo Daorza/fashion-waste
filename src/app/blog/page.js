09428e92212ef701e555004954de14d6b6853df0
Daorza <daokun04@gmail.com>
2025-03-08 10:40:45 +0700

    Merge file

---
+++ src/app/blog/page.js
@@ -0,0 +1,9 @@
+export default function blog() {
+    return (
+        <div className="bg-gray-100 flex flex-col min-h-screen">
+            <main className="flex-grow max-w-4xl mx-auto mt-10 p-8 bg-white shadow-xl rounded-xl">
+
+            </main>
+        </div>
+    );
+}
